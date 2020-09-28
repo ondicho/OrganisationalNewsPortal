c79ec2323e148431309b356b28757294dd9862d2
Ondicho <ondicho53@gmail.com>
2020-09-28 10:41:00 +0300

    this commit creates the object properties for the department Object

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -1,5 +1,53 @@
 package models;
 
-public class Department {
+import java.util.Objects;
 
+public class Department {
+    private int departmentId;
+    private String departmentName;
+    private String departmentDescription;
+
+    public Department(String departmentName,String departmentDescription){
+        this.departmentName=departmentName;
+        this.departmentDescription=departmentDescription;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public String getDepartmentDescription() {
+        return departmentDescription;
+    }
+
+    public void setDepartmentDescription(String departmentDescription) {
+        this.departmentDescription = departmentDescription;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return departmentId == that.departmentId &&
+                Objects.equals(departmentName, that.departmentName) &&
+                Objects.equals(departmentDescription, that.departmentDescription);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(departmentId, departmentName, departmentDescription);
+    }
 }
